9971154acbe14ae4916b6470122afaec2db2821c
Hoàng Quý <hoangquy@Hoangs-MacBook-Pro.local>
2026-03-27 16:39:38 +0700

    fix api interceptor, define mutation global

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,38 +1,9 @@
-import axios, { AxiosError, type AxiosResponse } from "axios";
-import { toast } from "react-toastify";
-import { extractApiErrorMessage } from "./apiError";
-import type { CustomAxiosRequestConfig } from "../types";
+import axios from "axios";
 
-const apiClient = axios.create({
+export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-// Global response interceptor
-apiClient.interceptors.response.use(
-  (response: AxiosResponse) => {
-    const config = response.config as CustomAxiosRequestConfig;
-
-    if (config.showSuccess) {
-      const message =
-        response.data?.message ?? "Operation successful!";
-      toast.success(message);
-    }
-
-    return response;
-  },
-  (error: AxiosError & { config?: CustomAxiosRequestConfig }) => {
-    const config = error.config as CustomAxiosRequestConfig;
-
-    if (!config?.silent) {
-      toast.error(extractApiErrorMessage(error));
-    }
-
-    return Promise.reject(error);
-  }
-);
-
-export default apiClient;
